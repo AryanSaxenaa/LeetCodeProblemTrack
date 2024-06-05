ffad3c0293235fc5f448047684a8fc3c2f767851
Aryan Saxena <aryansaxenaalig@gmail.com>
2024-06-05 08:39:59 +0530

    Time: 11 ms (41.65%) | Memory: 8.1 MB (78.98%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string pal = to_string(x);
-        string rev = pal;  
-        reverse(rev.begin(), rev.end());  
-        return pal == rev;
+        int y=x;
+        int ans=0;
+        int dummy;
+        while(y>0 && y< INT_MAX && y > INT_MIN){
+            dummy=y%10;
+            if (ans > (INT_MAX - dummy) / 10) {
+                return false; // Overflow would occur
+            }
+
+            ans=ans*10+dummy;
+            y=y/10;
+        }
+        if(ans==x){
+            return true;
+        }
+        return false;
     }
 };
